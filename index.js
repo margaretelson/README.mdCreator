bde9b6b9888b4f0a6774852a20316b59b6136691
Margaret Elson <margaret.a.elson@gmail.com>
2021-01-13 17:23:32 -0500

    Working on filling out the input questions to ask the user

--- index.js
+++ index.js
@@ -13,20 +13,42 @@ const fs = require('fs')
 inquirer.prompt([
     {
         type: 'input',
-        name:'name',
-        message: "What is your name?",
+        name: 'name',
+        message: 'What is your the name of your project?',
     },
     {
         type: 'input',
-        name: 'tvShow',
+        name:'Description',
+        message: "Please provide a description of your project.",
+    },
+    {
+        type: 'input',
+        name: 'Installation',
+        message: 'What are some step by step instructions on how to install your project?',
+    },
+    {
+        type: 'input',
+        name: 'Usage',
         message: 'What is your fave tv show?',
+    },
+    {
+        type: 'input',
+        name: 'Contributing',
+        message: 'What is your fave tv show?',
+    },
+    {
+        type: 'input',
+        name: 'Tests',
+        message: 'How do you run the automated tests for the system?',
     }
 ]).then((response) => {
     // console.log(response)
     const data = `
-##README
+##${resonse.name}
+
+##
     `
-    fs.writeFile('index.html', data, (error) => {
+    fs.writeFile('README.md', data, (error) => {
 
         error ? console.error(error) : console.log('success!')
     })
